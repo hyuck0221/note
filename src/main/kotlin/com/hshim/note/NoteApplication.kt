bf85fa6aa180da83bc327bd05416a00727dfa44e
심혁 <shimhyuck@simhyeog-ui-MacBookPro.local>
2025-05-13 16:12:51 +0900

    Timezone 설정

--- src/main/kotlin/com/hshim/note/NoteApplication.kt
+++ src/main/kotlin/com/hshim/note/NoteApplication.kt
@@ -3,11 +3,13 @@ package com.hshim.note
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.TimeZone
 
 @SpringBootApplication
 @EnableJpaAuditing
 class NoteApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<NoteApplication>(*args)
 }
